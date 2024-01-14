c57b4c7ba6a547b60d9926f7fe7e89ecbdc3ba76
mmwandera <mmwandera74@gmail.com>
2024-01-14 13:49:30 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
+# Define a Dog class in lib/dog.py. In the class, define an __init__ method that accepts an argument for the dog's name. That argument should be stored within a self.name attribute.
+# Additionally, Dog.__init__ should accept a second optional argument for the dog's breed stored in an attribute self.breed. When no breed is provided, it should default to "Mutt".
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
+# Define a Person class in lib/person.py. In the class, define an __init__ method that accepts an argument for the person's name. That argument should be stored within a self.name attribute.
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
